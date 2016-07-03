179685cf4479dd70881da7336ebb9775e073567c
Charlie on RohanElf <charlie@elvenware.com>
2016-07-03 15:44:28 -0700

    better image handling on mobile

--- ApacheHelpers/js/elven-help.js
+++ ApacheHelpers/js/elven-help.js
@@ -1,14 +1,18 @@
 $(document).ready(function() {
-    $('table').addClass('table table-striped table-hover');
+	'use strict';
+	$('table').addClass('table table-striped table-hover');
+	
+	$('#getPresidents').click(getPresidents);
 
-    $('#getPresidents').click(getPresidents);
-
-    function getPresidents() {
-        $.getJSON('/cgi-bin/get-presidents.py', function(presidents) {
-            presidents.forEach(function(president) {
-                $('#bar').append('<li>' + president[1] + ' ' + president[2] + '</li>');
-            });
-        })
-    }
-
+	function getPresidents() {
+		$.getJSON('/cgi-bin/get-presidents.py', function(presidents) {
+			presidents.forEach(function(president) {
+				$('#bar').append('<li>' + president[1] + ' ' + president[2] + '</li>');
+			});
+		})
+	}
+	
+	$('.navbar-nav li.trigger-collapse a').click(function(event) {
+        $('.navbar-collapse').collapse('hide');
+    });
 });
